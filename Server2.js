e2e6f0ccee98356166ff75ddb42a8320d22fbff9
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-04-17 19:33:02 -0700

    Server Code Cleaned Up w Instructions

--- Server2.js
+++ Server2.js
@@ -1,6 +1,14 @@
+//created the http call which allows server data to be stored
+
 var http = require('http');
 
+//specifies which port each server will use
+
 var PORT = 7000;
+
+var PORT2 = 7500;
+
+//functions that tell your server what to do when it is run
 
 function handleRequest(request, response) {
 
@@ -8,22 +16,24 @@ function handleRequest(request, response) {
 
 }
 
-var server = http.createServer(handleRequest);
-
-server.listen(PORT, function() {
-	console.log('Server listening on : http://localhost:' + PORT);
-})
-
-
-var PORT2 = 7500;
-
 function handleRequest2(request, response) {
 
 		response.end('You always look bad ' + request.url);
 
 }
 
+//create the actual servers and pass the functions that tell them what to do
+
+var server = http.createServer(handleRequest);
+
 var server2 = http.createServer(handleRequest2);
+
+
+//listener event that make the server listen to which port it is on and console log a path
+
+server.listen(PORT, function() {
+	console.log('Server listening on : http://localhost:' + PORT);
+})
 
 server2.listen(PORT2, function() {
 	console.log('Server listening on : http://localhost:' + PORT2);
